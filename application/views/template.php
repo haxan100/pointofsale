268e751a8e7b7defe559e9a236c4a0f9ffa04175
haxan100 <abdul.hasan388@gmail.com>
2020-06-06 06:05:15 +0700

    hapus beberapa file tidak pnting di template

--- application/views/template.php
+++ application/views/template.php
@@ -428,29 +428,7 @@ $bu = base_url();
 			<?php echo $contents ?>
 
 			<!-- Main content -->
-			<section class="content">
 
-				<!-- Default box -->
-				<div class="card">
-					<div class="card-header">
-						<h3 class="card-title">Title</h3>
-
-						<div class="card-tools">
-							<button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
-								<i class="fas fa-minus"></i></button>
-							<button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
-								<i class="fas fa-times"></i></button>
-						</div>
-					</div>
-					<!-- /.card-body -->
-					<div class="card-footer">
-						Footer
-					</div>
-					<!-- /.card-footer-->
-				</div>
-				<!-- /.card -->
-
-			</section>
 			<!-- /.content -->
 		</div>
 		<!-- /.content-wrapper -->
